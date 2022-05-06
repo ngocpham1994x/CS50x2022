be1bf1b5426593636983a11f5d98b7f3db734a0b
Ngoc Pham <61129869+ngocpham1994x@users.noreply.github.com>
2022-05-06 03:03:41 +0000

    completed version 2022

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-#include <cs50.h>
+
+#define MAX_LENGTH 100
+
+char buffer[MAX_LENGTH];   // storing name as buffer
 
 int main(void)
 {
-    string name = get_string("What is your name?\n");
-    printf("hello, %s\n",name);
+    printf("What's your name?\n");
+
+    fgets(buffer, sizeof(buffer), stdin);   // get user input
+
+    printf("hello, %s \n", buffer);
 }
