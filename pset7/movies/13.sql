4b72a8115b4588fa15f380ea67203541ccd267fa
Ngoc Pham <61129869+ngocpham1994x@users.noreply.github.com>
2022-08-13 20:53:55 +0000

    completed pset7

---
+++ pset7/movies/13.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT(name) FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.id IN
+    (SELECT movies.id FROM movies
+    JOIN stars ON stars.movie_id = movies.id
+    JOIN people ON people.id = stars.person_id
+    WHERE name = 'Kevin Bacon'
+    AND birth = 1958)
+EXCEPT
+    SELECT name FROM people
+    WHERE name = 'Kevin Bacon'
+    AND birth = 1958;
